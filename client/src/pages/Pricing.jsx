d122539d3469f45b25b69aed81c9dce1f3c34a97
Utkarsh Dashora <utkarshdashora@gmail.com>
2026-04-09 01:52:33 +0530

    Update Pricing.jsx

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import LoginModel from '../components/loginModel';
 import VantaBackground from '../components/VantaBackground';
 
-const serverUrl = "http://localhost:5000";
+const serverUrl = "https://ai-website-bulider1.onrender.com";
 
 function Pricing() {
     const navigate = useNavigate();
