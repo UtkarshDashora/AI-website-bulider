7fc9195f8bdc948701dd1628dfabdcbe71abca8a
Utkarsh Dashora <utkarshdashora@gmail.com>
2026-04-09 01:46:31 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const googleAuth = async (req, res) => {
         
         res.cookie("token", token, { 
             httpOnly: true, 
-            secure: process.env.NODE_ENV === "production", 
+            secure: true, 
             sameSite: "none", 
             maxAge: 7 * 24 * 60 * 60 * 1000 
         })
