cd1985dee14978afd4561334234942d8bd2adfe3
Utkarsh Dashora <utkarshdashora@gmail.com>
2026-04-09 12:30:10 +0530

    Import serverUrl from App in Pricing component

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import LoginModel from '../components/loginModel';
 import VantaBackground from '../components/VantaBackground';
-
+import { serverUrl } from '../App';
 const serverUrl = "https://ai-website-bulider1.onrender.com";
 
 function Pricing() {
